ac49dee184a8b97d2c8986b757f4e0296c1c5a2c
twodawg <mtwohey2@yahoo.com>
2014-11-06 22:20:59 -0800

    Initial commit

---
+++ Catalog1/Models/Product.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Catalog1.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductID { get; set; }
+
+        [DisplayName("Product Name")]
+        public string Name { get; set; }
+
+        public int Quantity { get; set; }
+
+        public int OrderMaximumQuantity { get; set; }
+
+        public Decimal Price { get; set; }
+
+        public string Description { get; set; }
+
+        public string ImageSmall { get; set; }
+
+        public string ImageLarge { get; set; }
+
+        public Decimal PriceModifier { get; set; }
+
+        public virtual ICollection<ProductReview> Reviews { get; set; }
+    }
+}
